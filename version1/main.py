d3e383aed3efde8c9a62c093e163785c3f7ea6b8
chris <christopherwiseman17@gmail.com>
2024-09-28 13:55:11 +0100

    Update main.py

--- version1/main.py
+++ version1/main.py
@@ -1,1 +1,10 @@
+import pyglet as py
+from files import engine
+
+def waves():
+    pass
+
+engine.window(900, 600, "Pyglet-Complement System Tower Defence",
+              resizable=False, fullscreen=False)
+py.app.run()
 
