028528134c722fa725cc715b2e9980cdf58e1649
chris <christopherwiseman17@gmail.com>
2024-09-28 14:03:54 +0100

    Update gui.py

--- version3/project_files/gui.py
+++ version3/project_files/gui.py
@@ -20,6 +20,33 @@ class button1(py.gui.PushButton):
 
 
 
+class Scroller:
+    def __init__(self, sprite, min_y, max_y):
+        self.x = sprite.x
+        self.y = sprite.y
+        self.sprite = sprite
+        self.min_y = min_y
+        self.max_y = max_y
+        self.width = sprite.width
+        self.height = sprite.height
+        self.drag = False
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        if self.x < x < self.x + self.width and \
+                self.min_y < y < self.max_y:
+            print("scroll")
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        if self.drag:
+            print("drag")
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if (x, y) in self.sprite:
+            self.drag = True
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        self.drag = False
+
 
 
 
